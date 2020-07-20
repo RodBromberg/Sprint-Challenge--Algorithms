63691045d0ec822f7f4168ae785e461fb1f75a9c
Rod <rod.bromberg@yahoo.com>
2020-07-19 21:27:14 -0400

    sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -95,7 +95,28 @@ class SortingRobot:
         return self._light == "ON"
 
     def sort(self):
-        pass
+        self.set_light_on()
+
+        while self.light_is_on():
+            self.set_light_off()
+
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+            self.swap_item()
+
+            while self.can_move_left():
+                self.swap_item()
+                self.move_left()
+                if self.compare_item() == -1:
+                    self.swap_item()
+                    self.set_light_on()
+            self.swap_item()
+            print(self._time)
 
 
 if __name__ == "__main__":
